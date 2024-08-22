98df1b9319fb05c8c79bdee72ef2602dabe9558f
Nadezhda Ilina <nadiailina9@gmail.com>
2024-08-22 11:44:55 +0300

    поправила тесты

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '../../../utils/test-utils';
+import { render, screen, fireEvent } from '../../utils/test-utils';
 import '@testing-library/jest-dom';
 
 import { Search } from './index';
